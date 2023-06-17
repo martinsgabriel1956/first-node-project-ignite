d7f6ede77c6d2d4a0d5f4cc3af0cc1c54e545689
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-06-16 21:12:59 -0300

    feat(server.js): create and List routes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,22 @@
 import http from "node:http";
 
-const listUser = (request, response) => response.end("Hello World!");
-const server = http.createServer(listUser);
+const listUser = (request, response) => {
+  const { method, url } = request;
 
+  const isUserURL = url === "/users";
+  const isListMethod = method === "GET" && isUserURL;
+  const isCreateMethod = method === "POST" && isUserURL;
+
+  if (isListMethod) {
+    return response.end("User list");
+  }
+
+  if (isCreateMethod) {
+    return response.end("Create user");
+  }
+
+  return response.end("Hello World!");
+};
+
+const server = http.createServer(listUser);
 server.listen(3333);
