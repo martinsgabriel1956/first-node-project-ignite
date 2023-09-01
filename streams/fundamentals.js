184869676f38b304505dea3f054e11d1673a0362
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-09-01 12:03:25 -0300

    feat(streams/fundamentals.js): write and transformation streams

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,4 +1,4 @@
-import { Readable } from "node:stream";
+import { Readable, Transform, Writable } from "node:stream";
 
 class OneToHundredStream extends Readable {
   index = 1;
@@ -16,4 +16,20 @@ class OneToHundredStream extends Readable {
   }
 }
 
-new OneToHundredStream().pipe(process.stdout);
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformedChunk = Number(chunk.toString()) * -1;
+    callback(null, Buffer.from(String(transformedChunk)));
+  }
+}
+
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10);
+    callback();
+  }
+}
+
+new OneToHundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream());
