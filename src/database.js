378c0faa370db0a6bf8f8193657f4ff0fe1330e9
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-09-01 16:36:52 -0300

    feat(database.js): persisting database

--- src/database.js
+++ src/database.js
@@ -1,6 +1,29 @@
+import fs from "fs";
+
+const databasePath = new URL("../database.json", import.meta.url);
+
 export class Database {
   #database = {};
 
+  constructor() {
+    fs.readFile(databasePath, (error, data) => {
+      if (error) {
+        this.#persist();
+      }
+
+      if (data) {
+        this.#database = JSON.parse(data);
+      } else {
+        this.#persist();
+      }
+    });
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database), (error) =>
+      console.log(error)
+    );
+  }
   select(table) {
     const data = this.#database[table] ?? [];
     return data;
@@ -12,6 +35,8 @@ export class Database {
     } else {
       this.#database[table] = [data];
     }
+
+    this.#persist();
 
     return data;
   }
