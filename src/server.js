76b1790a48493c135348cb6adfd674a4a2314a6c
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-09-01 14:35:55 -0300

    feat(src/server.js): request body in the JSON (stream & buffers)

--- src/server.js
+++ src/server.js
@@ -2,8 +2,20 @@ import http from "node:http";
 
 const users = [];
 
-const listUser = (request, response) => {
+const listUser = async (request, response) => {
   const { method, url } = request;
+
+  const buffers = [];
+
+  for await (const chunk of request) {
+    buffers.push(chunk);
+  }
+
+  try {
+    request.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch (error) {
+    request.body = null;
+  }
 
   const isUserURL = url === "/users";
   const isListMethod = method === "GET" && isUserURL;
@@ -16,10 +28,12 @@ const listUser = (request, response) => {
   }
 
   if (isCreateMethod) {
+    const { name, email } = request.body;
+
     const userBody = {
       id: 1,
-      name: "John Doe",
-      email: "johndoes@email.com",
+      name,
+      email,
     };
 
     users.push(userBody);
